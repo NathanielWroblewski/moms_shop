b27d25edaaa6a9449deec111b879a3cf7e6f9f2a
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2013-11-03 12:48:52 -0800

    new spree shop

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MumsShop::Application.config.secret_key_base = '27cfdac612d30c01fa3ed4658164ee3cc7e04e07802ed22957f814413f0e3a43c3d41f25e6200f147419b7e91077d9db4f5cec9a602b971975e3e681992a946b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MumsShop::Application.config.session_store :cookie_store, key: '_mums_shop_session'
