d7b9287597e920d4c475025b810ff43a05f7c8e5
Nathaniel Wroblewski <NathanielWroblewski@gmail.com>
2013-11-30 02:35:41 -0800

    precompile modernizr

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,8 @@ MumsShop::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( modernizr.js foundation-icons.woff
+    foundation-icons.ttf foundation-icon.svg )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
